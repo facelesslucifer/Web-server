b0a933d5cc994dd3b89552c3563b8603d7ded854
Ye Lin Htun <yelinhtun1987@gmail.com>
2015-12-19 10:13:13 +0800

    Initial commit

---
+++ middleware.js
@@ -0,0 +1,13 @@
+// Use module exports to expose middleware
+var middleware = {
+    requireAuthentication: function(request, response, next) {
+        console.log('Private route hit!');
+        next();
+    },
+    logger: function(request, response, next) {
+        console.log('Request: ' + new Date().toString() + ' ' + request.method + ' ' + request.originalUrl);
+        next();
+    }
+};
+
+module.exports = middleware;
---
+++ server.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var app = express();
+var PORT = 3000;
+
+var middleware = require('./middleware');
+
+app.use(middleware.logger);
+
+app.get('/about', middleware.requireAuthentication, function(request, response) {
+    response.send('About us');
+});
+
+app.get('/', function(request, response) {
+    response.send('Hello Express!');
+});
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(PORT, function() {
+    console.log('Express server is running on port ' + PORT);
+});
